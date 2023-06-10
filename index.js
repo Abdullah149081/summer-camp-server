0bbe2f685f2d1f710e81f061cd86e4da44de36a7
Abdullah149081 <masudmd425@gmail.com>
2023-06-10 23:09:33 +0600

    users api create

--- index.js
+++ index.js
@@ -65,6 +65,11 @@ async function run() {
     });
 
     // user api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
