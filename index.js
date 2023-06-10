7afd566edff3f80f53b3827b0eae11a41d4e301d
Abdullah149081 <masudmd425@gmail.com>
2023-06-10 20:30:00 +0600

    verigy jwt fnc create

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ignmh8y.mongodb.net/?retryWrites=true&w=majority`;
 
