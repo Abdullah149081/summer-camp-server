c7323a93d4e9cfa7234e7f407b515f7c2ca5ec27
Abdullah149081 <masudmd425@gmail.com>
2023-06-10 20:23:59 +0600

    banner api is done

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const bannerCollection = client.db("summerDB").collection("banner");
+
+    app.get("/banner", async (req, res) => {
+      const result = await bannerCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
