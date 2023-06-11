220d1fdc0912fbc9feb6554e15dede48b0f22389
Abdullah149081 <masudmd425@gmail.com>
2023-06-12 00:05:18 +0600

    approve class api is done

--- index.js
+++ index.js
@@ -140,6 +140,19 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/class/approve/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedClass = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateUser = {
+        $set: {
+          status: updatedClass.status,
+        },
+      };
+      const result = await classCollection.updateOne(filter, updateUser);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
